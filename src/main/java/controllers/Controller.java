02c2b40260c2c1dcedde4187c45db6cd1e23ea11
hannimpeha <hannimpeha@hanmail.net>
2020-08-29 23:46:31 +0900

    Fascinating Project

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Controller implements ActionListener {
 
@@ -64,6 +67,23 @@ public class Controller implements ActionListener {
                 break;
             case "Home":
                 mc.start();
+                break;
+            case "Testing":
+                test();
+                break;
         }
     }
+
+    private void test() {
+        List<String> names = new ArrayList<String>(Arrays.asList(
+                "Pierce","Mahsa","Christilyn","Elvin","Ronelle","Harry",
+                "Ron","Hermione","Tom Riddle","Albus D","Captain Jack","Sparkey Sparkey Boom Man"));
+        List<String> roles = new ArrayList<>(Arrays.asList(
+                "Doctor","Detective","Mafia: Hitman","Town","Bodyguard","Survivor",
+                "Mafia: Barman","Lyncher","Mafia- GodFather","Vigilante","Prostitute","Mafia: Goon"));
+
+        fu.newFile(names, roles);
+        gc.start(fu.getPlayerInfo());
+    }
+
 }
