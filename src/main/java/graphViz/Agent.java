ae46cb5c4b0caee7f60df4e865998f3855ab2422
HannahLee <hannimpeha@AmateurLife.local>
2020-09-12 13:35:54 +0900

    Fascinating Project

--- src/main/java/graphViz/Agent.java
+++ src/main/java/graphViz/Agent.java
@@ -1,8 +1,5 @@
 package graphViz;
 
-import jason.asSemantics.TransitionSystem;
-import jason.asSemantics.Unifier;
-import jason.asSyntax.Term;
 
 public class Agent extends jason.asSemantics.Agent {
 
@@ -10,9 +7,6 @@ public class Agent extends jason.asSemantics.Agent {
     private final String role;
     private final int status;
     private InternalAction ia;
-    private TransitionSystem ts;
-    private Unifier un;
-    private Term[] args;
 
     public Agent(String name, String role, int status){
         this.name = name;
