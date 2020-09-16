8be0f12bd12a6245badfe959cd8c1014cd031f01
HannahLee <hannimpeha@AmateurLife.local>
2020-09-16 10:18:34 +0900

    Fascinating Project

---
+++ src/main/java/FSM/IState.java
@@ -0,0 +1,15 @@
+package FSM;
+
+import java.util.Map;
+
+public interface IState {
+
+    Map<String, IState> getAdjacentStates();
+
+    String getStateDesc();
+
+    void addTransit(Action action, IState nextState);
+
+    void removeTransit(String targetStateDesc);
+}
+
